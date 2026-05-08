fa4176008f54dcbc2d8ae5ba7d54c0ccf285e0b2
Subhankar <maitisubhankar55@gmail.com>
2026-05-08 20:24:20 +0530

    Main layout prepared

--- Weather-Forecast/src/App.jsx
+++ Weather-Forecast/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import bgImage from './assets/BGimage.avif'
 import './App.css'
 
 function App() {
@@ -9,10 +7,36 @@ function App() {
 
   return (
     <>
-    <div className='bg-gray-300 w-full h-screen py-[2em]'>
-      <div className='w-[40%] h-[80vh] bg-blue-400 mx-auto rounded-md text-center text-amber-50 p-[1.5%] text-2xl font-semibold'>
-          Enter Location
+    <div className='w-full min-h-screen bg-cover bg-center bg-no-repeat'
+      style={{backgroundImage:`url(${bgImage})`}}
+    >
+      <div className='inset-0 py-[2em] bg-black/75'>
+          <div className='text-center mb-[1em] text-white font-medium text-5xl'>
+            Welcome to Weather App!
+          </div>
+          <div className='w-[40%] h-[80vh] bg-gray-700 opacity-80  mx-auto rounded-md pt-[2em]'>
+            <h3 className='text-center text-white text-2xl font-medium'>
+              Enter Location
+            </h3>
+            <form action="" method="post" className=''>
+              <input type="text" 
+              placeholder='Enter longitude'
+              className='bg-white w-[70%] block mx-auto my-[3em] border-gray-400 rounded-lg p-[0.5em] outline-none'
+              />
+              <input type="text" 
+              placeholder='Enter latitude'
+              className='bg-white w-[70%] block mx-auto my-[3em] border-gray-400 rounded-lg p-[0.5em] outline-none'
+              />
+              <button
+                type='submit'
+                className='block mx-auto my-[2em] py-[1em] px-[2.5em] rounded-lg shadow-md shadow-white/80 bg-green-900 text-center text-white font-medium hover:bg-green-800'
+              >
+                Search
+              </button>
+            </form>
+          </div>
       </div>
+      
     </div>
       
     </>
